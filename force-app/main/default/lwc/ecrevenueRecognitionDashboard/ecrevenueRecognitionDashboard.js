7766c0f29cc2a5352ba663ab81aed02a2bd632bb
rokuro881999 <anshulsimlote1999@gmail.com>
2025-02-08 20:04:01 +0530

    Implemented lightning tree grid #2

--- force-app/main/default/lwc/ecrevenueRecognitionDashboard/ecrevenueRecognitionDashboard.js
+++ force-app/main/default/lwc/ecrevenueRecognitionDashboard/ecrevenueRecognitionDashboard.js
@@ -2,27 +2,54 @@ import { LightningElement, wire, api, track} from 'lwc';
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import milestoneCalculation from '@salesforce/apex/EC_RevenueRecognitionController.milestoneCalculation';
 
-export default class RevenueRecognitionDashboard extends LightningElement {
+const COLUMNS = [
+    { 
+        label: 'Milestone / Product Name', 
+        fieldName: 'name', 
+        type: 'text'
+    },
+    { 
+        label: 'Recognized Revenue', 
+        fieldName: 'recognizedRevenue', 
+        type: 'currency'
+    },
+    { 
+        label: 'Invoice Status', 
+        fieldName: 'invoiceStatus', 
+        type: 'text'
+    }
+];
+export default class EcRevenueRecognitionDashboard extends LightningElement {
     @api recordId;
     @track oppDataObj = {};
+    @track revenueItems = [];
+    columns = COLUMNS;
     showSpinner = true;
-
-    @track columns = [
-        { label: 'Milestone', fieldName: 'milestoneName' },
-        { label: 'Milestone Date', fieldName: 'milestoneDate', type: 'date' },
-        { label: 'Recognized Revenue', fieldName: 'recognizedRevenue',  type: 'currency' },
-        { label: 'Invoice Status', fieldName: 'invoiceStatus' }
-        
-    ];
 
     @wire(milestoneCalculation, { opportunityId: '$recordId' })
     wiredMilestones({ error, data }) {
         if (data) {
             this.oppDataObj = JSON.parse(data);
+            this.revenueItems = this.transformData(JSON.parse(data));
         } else if (error) {
             this.showToastMessage('ERROR',error.body.message,'error');
         }
         this.showSpinner = false;
+    }
+
+    transformData(milestones) {
+        return milestones.revRecMilestonesList.map(milestone => ({
+            id: milestone.milestoneName,
+            name: milestone.milestoneName,
+            recognizedRevenue: milestone.recognizedRevenue,
+            invoiceStatus: milestone.invoiceStatus,
+            _children: milestone.revRecMilestonesItemList.map(product => ({
+                id: milestone.milestoneName + '-' + product.productName,
+                name: product.productName,
+                recognizedRevenue: product.TotalPrice,
+                invoiceStatus: ''
+            }))
+        }));
     }
 
     showToastMessage(title, message, variant){
